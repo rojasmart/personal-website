2b7389262d47326cad61b6423c3619cee37d56ff
rojasmart <rogeriosvaldo@gmail.com>
2025-08-15 14:18:46 +0100

    --update site name and title to reflect new branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://louai.site',
+  site: 'https://rojasmart.pt',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
